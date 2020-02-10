53eff1850785b10d386443319d391be474658b8c
Roger H Brock <rogerhbrock@gmail.com>
2020-02-10 07:59:26 -0500

    A few changes

--- rhb-app/src/index.js
+++ rhb-app/src/index.js
@@ -8,38 +8,6 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
-class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.state = {
-            isHome: true,
-            home: "",
-            isAbout: false,
-            isResume: false
-        };
-    }
-
-    handleClick() {
-  
-            if (this.state.isHome === false) {
-                this.setState({ isHome: true });
-            } else this.setState({ isHome: false });
-        
-
-    }
-
-    render() {
-        return (
-            <div>
-                <NavigationBar onClick={this.handleClick} />
-                <Home value={this.state.isHome} />
-                <About value={this.state.isAbout} />
-            </div>
-        );
-    }
-
-}
 
 
 
@@ -75,18 +43,25 @@ function About(props) {
 }
 
 
-function Resume() {
-    return (
-        <div className="">
-            <h1>Home</h1>
-        </div>
+function Resume(props) {
 
-    );
+    const x = props.value;
+
+    if (x) {
+        return (
+            <div className="">
+                <h1>Resume</h1>
+            </div>
+        );
+
+    } else return <div></div>;
 }
 
 function NavigationBar(props) {
 
     const home = "home";
+    const about = "about";
+    const resume = "resume";
 
     return (
         <div>
@@ -96,9 +71,9 @@ function NavigationBar(props) {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link value={home} onClick={props.onClick}>Home</Nav.Link>
-                        <Nav.Link onClick={props.onClick}>About</Nav.Link>
-                        <Nav.Link onClick={props.onClick}>Resume</Nav.Link>
+                        <Nav.Link value={home} onClick={(props.onClick(home))}>Home</Nav.Link>
+                        <Nav.Link value={about} onClick={props.onClick(about)}>About</Nav.Link>
+                        <Nav.Link value={resume} onClick={props.onClick(resume)}>Resume</Nav.Link>
                         {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -117,6 +92,73 @@ function NavigationBar(props) {
         </div>
 
     );
+}
+
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            isHome: true,
+            //home: "",
+            isAbout: false,
+            //about: "",
+            isResume: false,
+            //resume: ""
+        };
+    }
+
+    handleClick(props) {
+
+        const x = props.value;
+
+        // if (props.value = "home"){
+        //     if (this.state.isHome === false) {
+        //        return this.setState({ isHome: true });
+        //     } else this.setState({ isHome: false });
+        // }
+
+        if (x === "home"){
+            this.setState({ isHome: true });
+            this.setState({ isAbout: false });
+            this.setState({ isResume: false });
+        }
+
+        
+        if (x === "about"){
+            this.setState({ isHome: false });
+            this.setState({ isAbout: true });
+            this.setState({ isResume: false });
+        }
+        
+        if (x === "resume"){
+            this.setState({ isHome: false });
+            this.setState({ isAbout: false });
+            this.setState({ isResume: true });
+        }
+
+        // if (this.state.isAbout === false) {
+        //     this.setState({ isAbout: true });
+        // } else this.setState({ isAbout: false });
+
+        // if (this.state.isResume === false) {
+        //     this.setState({ isResume: true });
+        // } else this.setState({ isResume: false });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <NavigationBar onClick={this.handleClick} />
+                <Home value={this.state.isHome} />
+                <About value={this.state.isAbout} />
+                <Resume value={this.state.isResume} />
+            </div>
+        );
+    }
+
 }
 
 
